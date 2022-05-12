810849e24e5a08910a72c7875c5c55dee6f81aed
Nelson Cadenas <nca1478@yahoo.com>
2022-05-11 20:02:48 -0400

    updated: footer section (copyright)

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -4,7 +4,17 @@ export const Footer = () => {
   return (
     <div className="p-1 bg-dark text-white text-center">
       <div className="container">
-        <p className="lead pt-3">Copyright &copy; 2022 ToDo App</p>
+        <p className="lead pt-3">
+          Copyright &copy; 2022 - Developed by:{' '}
+          <a
+            href="https://nelsoncadenas.netlify.app/"
+            rel="noopener noreferrer"
+            className="text-primary text-decoration-none fw-bold"
+            target="_blank"
+          >
+            Nelson Cadenas
+          </a>
+        </p>
       </div>
     </div>
   )
